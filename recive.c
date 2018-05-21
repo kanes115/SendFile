8ca3feb9365258aa9b14c8dfc67871eb2a59b3c8
KrzysztofJopek <krzysztofjopek99@gmail.com>
2018-05-21 11:33:24 +0200

    Fix "adding num after extension" bug

--- recive.c
+++ recive.c
@@ -10,6 +10,7 @@
 
 int reciveFile(int sockfd);
 char* getName(char* name);
+int getNameExtPos(char* name);
 
 int main(int argc, char* argv[])
 {
@@ -56,13 +57,38 @@ int reciveFile(int sockfd)
 
 char* getName(char* name)
 {
-	char* buff = (char*)calloc(sizeof(char*), PATH_MAX+1);
+	char* buff = (char*)calloc(sizeof(char), PATH_MAX+1);
+	char* nm = (char*)calloc(sizeof(char), PATH_MAX);
+	char* ext = (char*)calloc(sizeof(char), PATH_MAX);
+	int pos = getNameExtPos(name);
+	strncpy(nm, name, pos);
+	strcpy(ext, name+pos);
 	strcpy(buff, name);
 	int i=1;
+	//Add number before extension
 	while(access(buff, F_OK) != -1){
-		sprintf(buff, "%s%d", name, i);
+		sprintf(buff, "%s%d%s", nm, i, ext);
 		i++;
 	}
+	free(nm);
+	free(ext);
 	return buff;
+}
+
+//return extension position in name
+int getNameExtPos(char* name)
+{
+	int pos = 0;
+	char c = *name;
+	int i=1;
+	for(; c!='\0'; i++){
+		c=name[i];
+		if(c == '.'){
+			pos=i;
+		}
+	}
+	if(pos==0)
+		return i-1;
+	return pos;
 }
 
